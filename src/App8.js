93ebe3d4e49bdb257db1742b3ac576813e4b6758
Jung2022 <quuu9999@gmail.com>
2022-04-27 10:18:35 +0900

    리액트 스터디2 끝

--- src/App8.js
+++ src/App8.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
@@ -7,14 +7,12 @@ import Navigation from './components/Navigation.js';
 
 function App8() {
   return (
-    <div>
+    <HashRouter>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} exact={true} />
-        <Route path="/about" element={<About />} />
-        <Route path="/movie-detail" element={<Detail />} />
-      </Routes>
-    </div>
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/about" component={About} />
+        <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
   );
 }
 
